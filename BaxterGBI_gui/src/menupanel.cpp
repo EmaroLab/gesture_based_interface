e885415fcfc4741a8234031d4d733ab57d1993cf
lucrezia <lucre.grassi@gmail.com>
2019-01-24 14:02:17 +0100

    Menu GUI implemented

--- BaxterGBI_gui/src/menupanel.cpp
+++ BaxterGBI_gui/src/menupanel.cpp
@@ -3,6 +3,7 @@
 
 #include <QDebug>
 #include <QLabel>
+#include <QVector>
 
 MenuPanel::MenuPanel(QWidget *parent) :
 	QWidget(parent),
@@ -17,6 +18,50 @@ MenuPanel::~MenuPanel(){
 
 void MenuPanel::updateMenuPanel(std::string m_title, std::vector<std::string> m_options,
 																std::vector<std::string> m_fixed_options, int8_t m_selection){
+	
 	QString title = QString::fromStdString(m_title);
 	ui->menuTitle->setText(title);
+	
+	//delete previous items in the menu, if present
+	QLayoutItem* item;
+	while ((item = ui->optionsContainer->layout()->takeAt(0)) != nullptr){
+		delete item->widget();
+    delete item;
+	}
+  while((item = ui->fixedOptionsContainer->layout()->takeAt(0)) != nullptr){
+		delete item->widget();
+    delete item;
+	}
+	optionsButtons.clear();
+	fixedOptionsButtons.clear();
+	
+	//add options buttons to scroll area
+	QVector<QString> options;
+	for(auto option : m_options){
+		auto text = QString::fromStdString(option);
+		options.append(text);
+		auto button = new QPushButton(text);
+		optionsButtons.push_back(button);
+		ui->optionsContainer->addWidget(button);		
+	}
+	
+	//vertical spacer
+	ui->optionsContainer->addStretch();
+	
+	//add fixed options buttons to bottom area
+	QVector<QString> fixedOptions;
+	for(auto fixedOption : m_fixed_options){
+		auto text = QString::fromStdString(fixedOption);
+		fixedOptions.append(text);
+		auto button = new QPushButton(text);
+		fixedOptionsButtons.push_back(button);
+		ui->fixedOptionsContainer->addWidget(button);		
+	}
+	
+	//manage selection
+	m_selection = m_selection % (optionsButtons.size() + fixedOptionsButtons.size());
+	if(m_selection >= 0 and m_selection < optionsButtons.size())
+		optionsButtons.at(m_selection)->setStyleSheet("QPushButton{ background-color: red; }");
+	else if (m_selection >= optionsButtons.size() and m_selection < (optionsButtons.size() + fixedOptionsButtons.size()))
+		fixedOptionsButtons.at(m_selection - options.size())->setStyleSheet("QPushButton{background-color: blue; }");
 }
