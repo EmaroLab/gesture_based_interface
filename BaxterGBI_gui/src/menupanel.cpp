8cfc1ba8a39151d65b60c3f0e47f178bc6889fb7
lucrezia <lucre.grassi@gmail.com>
2019-01-24 23:40:24 +0100

    Add icons to action panel + style

--- BaxterGBI_gui/src/menupanel.cpp
+++ BaxterGBI_gui/src/menupanel.cpp
@@ -20,8 +20,9 @@ void MenuPanel::updateMenuPanel(std::string m_title, std::vector<std::string> m_
 																std::vector<std::string> m_fixed_options, int8_t m_selection){
 	
 	QString title = QString::fromStdString(m_title);
+	ui->menuTitle->setStyleSheet(titleStyle);
 	ui->menuTitle->setText(title);
-	
+
 	//delete previous items in the menu, if present
 	QLayoutItem* item;
 	while ((item = ui->optionsContainer->layout()->takeAt(0)) != nullptr){
@@ -42,6 +43,7 @@ void MenuPanel::updateMenuPanel(std::string m_title, std::vector<std::string> m_
 		options.append(text);
 		auto button = new QPushButton(text);
 		optionsButtons.push_back(button);
+		button->setStyleSheet(style);
 		ui->optionsContainer->addWidget(button);		
 	}
 	
@@ -55,13 +57,15 @@ void MenuPanel::updateMenuPanel(std::string m_title, std::vector<std::string> m_
 		fixedOptions.append(text);
 		auto button = new QPushButton(text);
 		fixedOptionsButtons.push_back(button);
+		button->setStyleSheet(style);
+		//button->setStyleSheet(buttonStyle);
 		ui->fixedOptionsContainer->addWidget(button);		
 	}
 	
 	//manage selection
 	m_selection = m_selection % (optionsButtons.size() + fixedOptionsButtons.size());
 	if(m_selection >= 0 and m_selection < optionsButtons.size())
-		optionsButtons.at(m_selection)->setStyleSheet("QPushButton{ background-color: red; }");
+		optionsButtons.at(m_selection)->setStyleSheet(selectedStyle);
 	else if (m_selection >= optionsButtons.size() and m_selection < (optionsButtons.size() + fixedOptionsButtons.size()))
-		fixedOptionsButtons.at(m_selection - options.size())->setStyleSheet("QPushButton{background-color: blue; }");
+		fixedOptionsButtons.at(m_selection - options.size())->setStyleSheet(selectedStyle);
 }
