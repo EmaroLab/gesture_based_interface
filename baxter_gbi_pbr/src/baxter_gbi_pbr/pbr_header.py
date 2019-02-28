690b79ac3a268fab922e543e2e87fb8be00203e5
Alessio De Luca <alessio.deluca.iic96d@gmail.com>
2019-02-28 16:07:53 +0100

    Nodes for mirroring offline method.

--- baxter_gbi_pbr/src/baxter_gbi_pbr/pbr_header.py
+++ baxter_gbi_pbr/src/baxter_gbi_pbr/pbr_header.py
@@ -110,8 +110,7 @@ class PlaybackObj(object):
             while ((number_lines < len(lines)-1) and self.stop == 0):
                 if service and service.is_preempt_requested(): break
                 feedback.percent_complete = (number_lines/float((len(lines)-1))*100.0)/float(loops - l + 1)
-                #rospy.loginfo("Complete:"+str(feedback.percent_complete))
-                rospy.loginfo("Preempted :"+str(service.is_preempt_requested()))
+                rospy.loginfo("Complete:"+str(feedback.percent_complete))
                 service.publish_feedback(feedback)
 
                 if self.pause_state == 0:
