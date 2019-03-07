325b91cc9ebd093356a7fe991aba0679249ba1a9
Patrick Roncagliolo <ronca.pat@gmail.com>
2019-03-07 16:00:15 +0100

    Fix scripts

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 cd ~/sofar_ws
 catkin_make | tee last_build.log
 source devel/setup.bash
-cp -f src/imu_wear/android_wear_pub/build/outputs/apk/debug/android_wear_pub-debug.apk imu_wear-debug.apk
-cp -f src/beacons_proximity/android_wear_pub/build/outputs/apk/debug/android_wear_pub-debug.apk beacons_proximity-debug.apk
+cp -f src/beacons_proximity/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk beacons_proximity.apk
+cp -f src/imu_wear/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk imu_wear.apk
