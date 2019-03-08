0c663202295ac7c157da19e448403b4becd3fee2
Patrick Roncagliolo <ronca.pat@gmail.com>
2019-03-08 09:37:56 +0100

    Minor fixes

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd ~/sofar_ws
+mkdir -p apk_release
 catkin_make | tee last_build.log
 source devel/setup.bash
-cp -f src/beacons_proximity/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk beacons_proximity.apk
-cp -f src/imu_wear/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk imu_wear.apk
+cp -f src/beacons_proximity/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk apk_release/beacons_proximity.apk
+cp -f src/imu_wear/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk apk_release/imu_wear.apk
--- clean_build.sh
+++ clean_build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 cd ~/sofar_ws
-rm -rf build devel
+rm -rf build devel apk_release
+mkdir apk_release
 find . -name "*.apk" -exec rm -f {} \;
 catkin_make -DCATKIN_WHITELIST_PACKAGES="rosjava_messages;genjava;rosjava_build_tools"  | tee last_build.log
 catkin_make -DCATKIN_WHITELIST_PACKAGES="imu_wear;beacons_proximity_sub" -j1 | tee -a last_build.log
 yes | sdkmanager --licenses
 catkin_make -DCATKIN_WHITELIST_PACKAGES=""  | tee -a last_build.log
 source devel/setup.bash
-cp -f src/beacons_proximity/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk beacons_proximity.apk
-cp -f src/imu_wear/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk imu_wear.apk
+cp -f src/beacons_proximity/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk apk_release/beacons_proximity.apk
+cp -f src/imu_wear/android_wear_pub/build/outputs/apk/release/android_wear_pub-release-unsigned.apk apk_release/imu_wear.apk
