ca9560f20b6514afe95fa1fcdf7acaa6d1702773
Unknown <carfi.alessandro@gmail.com>
2019-03-09 15:15:48 +0100

    Minor fix

--- imu_wear/android_wear_pub/src/com/github/emaro/imu_wear/SendingWearActivity.java
+++ imu_wear/android_wear_pub/src/com/github/emaro/imu_wear/SendingWearActivity.java
@@ -38,6 +38,11 @@ public class SendingWearActivity extends RosWearActivity implements SensorEventL
     setAmbientEnabled();
     frequency = findViewById(R.id.frequency);
     frequency.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {}
       /**
        * Function triggered when the frequency EditText is modified
        */
