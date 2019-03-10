b2095e69ac3a850ca4788ee23d22a2dabd97e61b
Patrick Roncagliolo <ronca.pat@gmail.com>
2019-03-10 19:32:10 +0100

    Update clean_build.sh

--- clean_build.sh
+++ clean_build.sh
@@ -4,7 +4,7 @@ rm -rf build devel apk_release
 mkdir apk_release
 find . -name "*.apk" -exec rm -f {} \;
 catkin_make -DCATKIN_WHITELIST_PACKAGES="rosjava_messages;genjava;rosjava_build_tools"  | tee last_build.log
-catkin_make -DCATKIN_WHITELIST_PACKAGES="imu_wear;beacons_proximity_sub" -j1 | tee -a last_build.log
+catkin_make -DCATKIN_WHITELIST_PACKAGES="imu_wear;beacons_proximity" -j1 | tee -a last_build.log
 yes | sdkmanager --licenses
 catkin_make -DCATKIN_WHITELIST_PACKAGES=""  | tee -a last_build.log
 exec bash
